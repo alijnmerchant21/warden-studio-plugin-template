6e803f77f59a7d6647cc286056587ff8aae9bd14
Aliasgar Merchant <amercha1@stevens.edu>
2025-06-17 23:15:22 +0200

    Add echo

--- src/Contract.sol
+++ src/Contract.sol
@@ -5,19 +5,8 @@ pragma solidity ^0.8.19;
 /// @notice A template for creating Warden Studio plugins
 contract WardenPlugin {
     // Plugin metadata
-    string public name;
-    string public description;
-    string public version;
 
-    constructor(
-        string memory _name,
-        string memory _description,
-        string memory _version
-    ) {
-        name = _name;
-        description = _description;
-        version = _version;
-    }
+    constructor()
 
     // Add your plugin logic here
 }
